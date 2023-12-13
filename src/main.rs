11d841bf6af207c1898bcc2e94bdc8f988e5d1d1
Alex Pitsikoulis <alex.pitsikoulis@gmail.com>
2023-12-13 09:52:41 -0800

    added main read loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Read, Write, Result, ErrorKind};
+
+const CHUNK_SIZE: usize = 16 * 1024;
+
+fn main() -> Result<()> {
+    let silent = !std::env::var("PV_SILENT").unwrap_or_default().is_empty();
+    let mut total_bytes = 0;
+    let mut buf = [0; CHUNK_SIZE];
+    loop {
+        let num_read = match io::stdin().read(&mut buf) {
+            Ok(0) => break,
+            Ok(x) => Ok(x),
+            Err(e) => Err(e),
+        }?;
+        total_bytes += num_read;
+        if !silent {
+            eprint!("\rbytes read: {}", total_bytes);
+        }
+        if let Err(e) = io::stdout().write_all(&buf[..num_read]) {
+            if e.kind() == ErrorKind::BrokenPipe {
+                break;
+            }
+            return Err(e);
+        }
+    }
+    Ok(())
 }
